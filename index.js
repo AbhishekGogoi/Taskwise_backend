0185cc920468a00662003f0d3bf4885fa3b3a8eb
Abhishek Gogoi <abhisat495@gmail.com>
2024-05-17 21:09:12 +0530

    added default root

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express=require("express");
-const connectDB = require('./config/db.config');
+const express = require("express");
+const connectDB = require("./config/db.config");
 
-
-const app=express();
+const app = express();
 // parse requests of content-type - application/json
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const PORT =8080;
-app.listen(PORT, () => { console.log('Server is running on port 8080') });
-
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log("Server is running on port 8080");
+});
+app.get("/", (req, res) => {
+  return res.status(204).send("Welcome to BookStore Application");
+});
 
 //  Connect to MongoDB
 connectDB();
