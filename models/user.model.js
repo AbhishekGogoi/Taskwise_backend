998d165d37fdac60c5dbd3b0425a8a134c2efa01
Navina K <navinak321@gmail.com>
2024-05-27 18:16:34 +0530

    registration issues solved

--- models/user.model.js
+++ models/user.model.js
@@ -1,20 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-const userModel = mongoose.model("users", userSchema);
-module.exports = userModel;
+module.exports = mongoose => {
+    var schema = new mongoose.Schema({
+      username: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+    },
+        { timestamps: true }
+    );
+    schema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+
+    const User = module.exports = mongoose.model('User', schema);
+    return User;
+}
+
