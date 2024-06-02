509bb18da70470ddb3de02bfbbbd6017f59a11de
Abhishek Gogoi <abhisat495@gmail.com>
2024-06-02 16:34:47 +0530

    commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,27 +1,54 @@
 module.exports = (app) => {
+  const passport = require("../utils/passport");
+  // const passport = require("passport");
+
   const {
     registerUser,
     loginUser,
     signOutUser,
+    forgotPassword,
+    verifyResetCode,
+    resendOTP,
+    resetPassword,
   } = require("../controllers/auth.controller.js");
-
-  const {
-    userRegisterValidate,
-    userLoginValidate,
-  } = require("../utils/userValidations.js");
-
-  //   const { ensureAuthenticated } = require("../utils/auth.js");
 
   const router = require("express").Router();
 
   // Register a new user
-  router.post("/auth/register", userRegisterValidate, registerUser);
+  router.post("/auth/register", registerUser);
 
   // Login a user
-  router.post("/auth/login", userLoginValidate, loginUser);
+  router.post("/auth/login", loginUser);
 
   // Logout a user
   router.post("/auth/logout", signOutUser);
+
+  //Google OAuth routes
+  router.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+  );
+
+  router.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/" }),
+    (req, res) => {
+      // Successful authentication, redirect to client
+      res.redirect("http://localhost:3000/projects"); // Adjust the URL as needed
+    }
+  );
+
+  // Route to handle forgot password (send reset code)
+  router.post("/auth/forgotpassword", forgotPassword);
+
+  // Route to handle verification of the reset code
+  router.post("/auth/verification", verifyResetCode);
+
+  // Route to handle resending the OTP
+  router.post("/auth/resendotp", resendOTP);
+
+  // Route to handle resetting the password
+  router.post("/auth/resetpassword", resetPassword);
 
   app.use("/api", router);
 };
