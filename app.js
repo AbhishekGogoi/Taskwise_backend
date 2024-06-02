7c6ef5d0d40354f5cb2a5969deb21fdbf84b9325
Abhishek Gogoi <abhisat495@gmail.com>
2024-06-02 16:40:18 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("./utils/passport");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 
@@ -13,20 +16,28 @@ app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.S3_BUCKET || !process.env.MONGODB_URL) {
-  console.error('Environment variables not set properly.');
+if (
+  !process.env.AWS_ACCESS_KEY_ID ||
+  !process.env.AWS_SECRET_ACCESS_KEY ||
+  !process.env.S3_BUCKET ||
+  !process.env.MONGODB_URL
+) {
+  console.error("Environment variables not set properly.");
   process.exit(1);
 }
 
 db.mongoose
-  .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => {
-    console.error('MongoDB connection error:', err);
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   });
 
-app.use('/api/upload', uploadController);
+app.use("/api/upload", uploadController);
 // Import routes
 require("./routes/project.routes")(app);
 require("./routes/workspace.routes")(app);
@@ -38,11 +49,12 @@ const swaggerOptions = {
     info: {
       title: "TaskWise API",
       version: "1.0.0",
-      description: "TaskWise is a task management API that allows users to manage workspaces, projects, tasks efficiently.",
+      description:
+        "TaskWise is a task management API that allows users to manage workspaces, projects, tasks efficiently.",
     },
-    servers: [{ url: "http://localhost:8080" }]
+    servers: [{ url: "http://localhost:8080" }],
   },
-  apis: ["./controllers/*.js", "./routes/*.js"]
+  apis: ["./controllers/*.js", "./routes/*.js"],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
