749b085bec76665aecd70d5f2f237a79e535c278
Abhishek Gogoi <abhisat495@gmail.com>
2024-06-02 16:35:47 +0530

    commit

--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,38 @@
+// const jwt = require("jsonwebtoken");
+// const ensureAuthenticated = (req, res, next) => {
+//   if (!req.headers["authorization"]) {
+//     return res.status(403).json({ message: "Token is required" });
+//   }
+//   try {
+//     const decoded = jwt.verify(
+//       req.headers["authorization"],
+//       process.env.SECRET
+//     );
+//     return next();
+//   } catch (err) {
+//     return res
+//       .status(403)
+//       .json({ message: "Token is not valid, or it's expired" });
+//   }
+// };
+
+// module.exports = {
+//   ensureAuthenticated,
+// };
+
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const ensureAuthenticated = (req, res, next) => {
-  if (!req.headers["authorization"]) {
+  const token = req.cookies.access_token; // Extract the token from cookies
+
+  if (!token) {
     return res.status(403).json({ message: "Token is required" });
   }
+
   try {
-    const decoded = jwt.verify(
-      req.headers["authorization"],
-      process.env.SECRET
-    );
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Attach the decoded user information to the request
     return next();
   } catch (err) {
     return res
